0b7a9a94dc74a234436e5f1529cf924f76b0a988
NITIN <nitnkohli@gmail.com>
2024-03-14 01:29:38 -0400

    Corrected Rest to REST

--- src/test/java/RESTAssured.java
+++ src/test/java/RESTAssured.java
@@ -1,6 +1,6 @@
 public class RESTAssured {
     public static void main(String[] args) {
 
-        System.out.println("This is my practice for rest assured");
+        System.out.println("This is my practice for Rest assured");
     }
 }
